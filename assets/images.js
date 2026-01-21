38cf0e81b4b63559b0c3ab4eaf06d5dc19419998
Jamuna. K <jamuna9495@gmail.com>
2026-01-21 22:49:26 +0530

    Add image tag for candidate card

---
+++ assets/images.js
@@ -0,0 +1,4 @@
+// Candidate card kulla intha image tag-ah podunga
+<img src="https://ui-avatars.com/api/?name=${p.name}&background=random&color=fff" 
+     alt="${p.name}" 
+     style="width: 45px; height: 45px; border-radius: 12px; margin-right: 15px;">
